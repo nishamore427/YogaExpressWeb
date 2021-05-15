1c607add1722ed8f7da92d19f385f50d00e91336
Nishigandha Dwarkadas More <68110728+nishamore427@users.noreply.github.com>
2021-05-15 15:08:53 +0530

    nodeJs Express project commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const hbs =  require('hbs');
+const app = express();
+const port = process.env.PORT || 8000;
+
+// public static path
+
+const static_path = path.join(__dirname,'../public');
+const template_path = path.join(__dirname,'../templates/views');
+const partials_path = path.join(__dirname,'../templates/partials');
+app.set('view engine','hbs');
+app.set('views',template_path);
+app.use(express.static(static_path));
+hbs.registerPartials(partials_path)
+
+// routing
+app.get("",(req,res)=>{
+    res.render("index");
+})
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+app.get("*",(req,res)=>{
+    res.render("404errorPage",{
+        errorMsg:'Opps! Page Not Found'
+    });
+})
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+})
